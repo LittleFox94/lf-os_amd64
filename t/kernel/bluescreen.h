c555b4556a8bfa6857d5ee21d8d7b9d7ab72b53c
Mara Sophie Grosch <littlefox@lf-net.org>
2019-09-04 23:30:07 +0200

    Fix tests: panic functions

---
+++ t/kernel/bluescreen.h
@@ -0,0 +1,2 @@
+__attribute__((noreturn)) static void panic_message(const char* message) { fprintf(stderr, "Panic() called: %s\n", message); exit(-1); }
+__attribute__((noreturn)) static void panic() { panic_message("Unknown error"); }
