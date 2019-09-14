13a47ac873a432d02c0b13f5f6016e81e4a18343
Mara Sophie Grosch <littlefox@lf-net.org>
2019-09-15 01:58:52 +0200

    More useful _start (not calling 0x1() anymore)

--- src/kernel/arch/amd64/start.c
+++ src/kernel/arch/amd64/start.c
@@ -1,12 +1,13 @@
 #include "../../../loader/loader.h"
+#include "bluescreen.h"
 
 void main(LoaderStruct* loaderStruct);
 
 void _start(LoaderStruct* loaderStruct) {
     // remove stack base before loader for clean stack traces
     //   (invalid anyway)
-    asm("mov $0, %rbp");
+//    asm("mov $0, %rbp");
 
     main(loaderStruct);
-    ((void(*)())1)();
+    panic_message("main() returned! WTF");
 }
