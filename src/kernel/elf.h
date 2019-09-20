671e4dc6dcdb9467528dded9f631d33bd6c4f49a
Mara Sophie Grosch <littlefox@lf-net.org>
2019-09-20 22:38:19 +0200

    ELF: add docs

--- src/kernel/elf.h
+++ src/kernel/elf.h
@@ -6,6 +6,7 @@
 
 #define ELF_MAGIC 0x464c457f
 
+//! Header of ELF images
 typedef struct {
     uint32_t ident_magic;
     uint8_t  ident_arch;
@@ -37,6 +38,7 @@ typedef struct {
     uint16_t sectionHeaderSectionNameIndex;
 }__attribute__((packed)) elf_file_header_t;
 
+//! Program headers in ELF images, required for preparing a program for execution
 typedef struct {
     uint32_t type;
     uint32_t flags;
@@ -50,6 +52,7 @@ typedef struct {
     uint64_t align;
 }__attribute__((packed)) elf_program_header_t;
 
+//! Section headers in ELF images, required to read additional information from file
 typedef struct {
     uint32_t name;
     uint32_t type;
@@ -63,6 +66,7 @@ typedef struct {
     uint64_t entrySize;
 }__attribute__((packed)) elf_section_header_t;
 
+//! Symbols in ELF images, there may be many
 typedef struct {
     uint32_t name;
     uint8_t  info;
@@ -72,8 +76,23 @@ typedef struct {
     uint64_t size;
 }__attribute__((packed)) elf_symbol_t;
 
+/**
+ * Parse ELF file, map program segments to context and return proper location for stack.
+ *
+ * \param[in]  elf     Memory chunk where the ELF file is loaded.
+ * \param[in]  context VM context for mapping of program segments.
+ * \param[out] stack   Writes proper location for stack in the variable pointed to by this pointer.
+ * \returns            Entrypoint of the image
+ */
 ptr_t load_elf(ptr_t elf, vm_table_t* context, ptr_t* stack);
 
+/**
+ * Return section header of ELF file by name
+ *
+ * \param name Name of the section to return
+ * \param elf  Loaded ELF image in memory
+ * \returns    Pointer to elf_section_header_t if found, NULL otherwise
+ */
 elf_section_header_t* elf_section_by_name(const char* name, const void* elf);
 
 #endif
