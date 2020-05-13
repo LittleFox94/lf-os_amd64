47f97a42702317a9eae247d89fef2bc04c96cec6
Mara Sophie Grosch <littlefox@lf-net.org>
2020-05-13 16:31:16 +0200

    Also add allocator and ssize_t ...

---
+++ src/kernel/allocator.h
@@ -0,0 +1,9 @@
+#ifndef _ALLOCATOR_H_INCLUDED
+#define _ALLOCATOR_H_INCLUDED
+
+#include <stdint.h>
+
+typedef void*(*allocator_t)(size_t size);
+typedef void(*deallocator_t)(void* ptr);
+
+#endif
--- src/kernel/arch/amd64/arch.h
+++ src/kernel/arch/amd64/arch.h
@@ -16,6 +16,7 @@ typedef unsigned long uint64_t;
 
 typedef uint64_t ptr_t;
 typedef uint64_t size_t;
+typedef int64_t  ssize_t;
 
 #define HIGHER_HALF_START 0xFFFF800000000000
 
