bd5ad5c477a0fbd0d07ff2b47fee605729b41fb0
Mara Sophie Grosch <littlefox@lf-net.org>
2020-10-15 19:18:25 +0200

    cmake: make image size configurable
    overflowed with debug-qt-init x)

--- cmake/images.cmake
+++ cmake/images.cmake
@@ -1,6 +1,11 @@
+set(image_size_hd 256 CACHE STRING "hd.img size")
+
+math(EXPR image_size_hd_sectors "${image_size_hd} * 1024 * 1024 / 512")
+math(EXPR boot_fs_sectors "${image_size_hd_sectors} - 2048")
+
 add_custom_target(hd.img
-    COMMAND dd if=/dev/zero of=hd.img bs=512 count=133156
-    COMMAND /sbin/sgdisk -Z -o -n 1:2048:133120 -t 1:ef00 hd.img
-    COMMAND mformat -i hd.img@@1M -F -L 131072
+    COMMAND dd if=/dev/zero of=hd.img bs=512 count=${image_size_hd_sectors}
+    COMMAND /sbin/sgdisk -Z -o -n 1:2048:${image_size_headers} -t 1:ef00 hd.img
+    COMMAND mformat -i hd.img@@1M -F -L ${boot_fs_sectors}
     COMMAND mcopy   -i hd.img@@1M -sbnmv ${CMAKE_BINARY_DIR}/shared/* ::/
 )
