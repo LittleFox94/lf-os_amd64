211178d826f7ab172bfa102193afbd6fc539bc6b
Mara Sophie Grosch <littlefox@lf-net.org>
2021-01-22 02:24:24 +0100

    More errno´s

--- src/include/errno.h
+++ src/include/errno.h
@@ -1,9 +1,34 @@
 #ifndef _ERRNO_H_INCLUDED
 #define _ERRNO_H_INCLUDED
 
-#define EPERM   1
-#define ENOMEM 12
-#define EBUSY  16
-#define EINVAL 22
+#define EPERM         1
+#define ENOENT        2
+#define ESRCH         3
+#define EBADF         9
+#define ECHILD       10
+#define EAGAIN       11
+#define ENOMEM       12
+#define EACCES       13
+#define EBUSY        16
+#define EEXIST       17
+#define ENOTDIR      20
+#define EINVAL       22
+#define ENFILE       23
+#define EFBIG        27
+#define ENOSPC       28
+#define ESPIPE       29
+#define EROFS        30
+#define EMLINK       31
+#define EDOM         33
+#define ERANGE       34
+#define ENAMETOOLONG 36
+#define ENOSYS       38
+#define ENOMSG       42
+#define EOVERFLOW    75
+#define EMSGSIZE     90
+#define ENOTSUP      95
+
+// user defined after this
+#define __ELASTERROR 2000
 
 #endif
