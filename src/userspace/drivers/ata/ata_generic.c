66aac56d0ac605d2ae073f176d4896652f265351
k4m1 <no>
2021-02-01 16:10:08 +0200

    Smol update

--- src/userspace/drivers/ata/ata_generic.c
+++ src/userspace/drivers/ata/ata_generic.c
@@ -9,7 +9,11 @@
  * Author: k4m1  <k4m1\protonmail.com>
  */
 
+#include <sys/syscalls.h>
 #include <sys/io.h>
+
+#include <stdio.h>
+#include <stdint.h>
 
 #include "ata.h"
 
@@ -33,242 +37,243 @@ unsigned char ata_delay_in(short port) {
 }
 
 /**
- * Helper to check ata disk status
+ * Helper to perform manual cache flushes. 
+ * In case drive does not do cache flush, it's up to us to handle that,
+ * not flushnig the cache might lead to writes invisibly failing, 
+ * bad sectors being created, etc.. fun stuff.
  *
- * \param port short Is IO port to device status register
- * \return 1 if device is ready, or 0 if busy
+ * \param port short Is IO base
  */
-int ata_device_ready(short port) {
-    int status;
+inline void ata_cache_flush(short port) {
+    outb(__ATA_STAT(port), __ATA_CMD_CFLUSH);
+}
 
-    status = ata_delay_in(port);
-    if ((status & __ATA_STAT_RDY) && ((status & __ATA_STAT_WAIT) == 0)) {
-        return 1;
+
+/**
+ * Check if the disk detected is sata or atapi.
+ *
+ * \param port short Is IO Base to ata device to check
+ * \return int disk type, 3 for sata or 4 for atapi, or 2 for some form of
+ * non spec-following cursed ATA disk.
+ */
+int ata_check_if_sata_or_atapi(short port) {
+    unsigned short disk_id;
+
+    disk_id = (inb(__ATA_CYL_LO(port)) << 8);
+    disk_id |= inb(__ATA_CYL_HI(port));
+
+    if (disk_id == 0x3c3c) {
+        return __ATA_DISK_ID_INTERNAL_SATA;
+    } else if (disk_id == 0x14EB) {
+        return __ATA_DISK_ID_INTERNAL_ATAPI;
+    } else {
+        return __ATA_DISK_ID_INTERNAL_ATA;
     }
-    return 0;
 }
 
 /**
- * Waiter for ata device to become ready, we will trigger error to indicate
- * user if disk timed out, TODO: figure out ways to print stuff
+ * Helper for triggering device reset.
+ * This is generally needed if device firmware has crashed or hung, or
+ * has othervice become unresponsive.
  *
- * \param port short Is IO port to device status register
- * \return 0 on success or -1 on timeout
+ * \require port short Is IO base for device to reset
  */
-int ata_device_wait(short port)
-{
-    int counter;
-    int ret = -1;
-    int reset_done = 0;
-    
-start:
-    for (counter = 0xFFFF; counter > 0; counter--) {
-        if (ata_device_ready(port)) {
-            ret = 0;
-            break;
-        }
-    }
-    if (!reset_done) {
-        ata_sw_reset(port);
-        reset_done = 1;
-        goto start;
-    }
+void ata_reset_device(short port) {
+    unsigned char iobyte;
 
-    return ret;
+    /* Read status bits, issue reset command, and write back */
+    iobyte = inb(__ATA_STAT(port));
+    iobyte |= __ATA_DCR_RST;
+    outb(__ATA_STAT(port), iobyte);
+
+    asm volatile("nop;");
+    asm volatile("nop;");
+
+    /* unset reset bit, and write back original content */
+    iobyte ^= (1UL << __ATA_DCR_RST);
+    outb(__ATA_STAT(port), iobyte);
 }
 
 /**
- * Do manual cache flush - as some drives require.
- * In case the drive does not do this, temporary bad sectors may be created,
- * or the subsequent writes might fail invisibly. Be careful with this one!
+ * Non standard ways to detect ATA disks:
+ *  1.) Try with DD command (0x90), if issuing DD cmd sets
+ *      error/feature register to all 1's (0xFF), then this is 
+ *      ATA disk.
  *
- * \param port short Is a IO base + offset to command register
- * \return 0 on success or non-zero on error
+ *  2.) Select the disk, and then check if bit 7 in status register (wait)
+ *      is set. 
+ *
  */
-int ata_cache_flush(short port) {
-    int counter;            // Used to poll in until clear flag is set
-    int ret = -1;
-
-    if (ata_device_wait(port)) {
-        return ret;
-    }
-
-    outb(0xE7, port);
-    for (counter = 0; counter < 50; counter++) {
-        if ((inb(port) & __ATA_STAT_WAIT) == 0) {
-            ret = 0;
-            break;
-        }
-    }
-    return ret;
-}
 
 /**
- * Perform software reset to ATA device, this could prove useful on some
- * errors etc. (Have you tried turning it off and on again)
+ * Detect ata disk by disk select
  *
- * The short NOP delay is to give slower ATA disks time to react.
- *
- * \param port short Is control register of this bus
- * \return Nothing.
+ * \param port short Is IO base to device to check for
+ * \param disk_num int Is disk number, (0/1)
+ * \return int result, 2 for ata, 0 for no disk.
  */
-void ata_sw_reset(short port) {
-    unsigned short cmd_status_byte;
-
-    /* Read in status register, OR it by 4 to set SRST = 4, then write back */
-    cmd_status_byte = inb(port);
-    cmd_status_byte |= 0x4;
-    outb(port, cmd_status_byte);
-
-    /* delay to let reset happen */
-    NOP();
-    NOP();
-
-    /* Clear SRST bit, and write command back */
-    cmd_status_byte &= 0xfb;
-    outb(port, cmd_status_byte);
-}
-
-/**
- * Check for floating bus by reading status register.
- * As status reg bits 1 and 2 should always be 0, them being 1's tells us
- * that something is wrong. Additionally, if we only read 1's back and end
- * up with status register value FF, we know bus floats.
- *
- * \param port short Is a status register to bus you want to check
- * \return non-zero if bus floats, or 0 if bus does not float.
- */
-int ata_check_bus(short port) {
-    if (inb(port) == 0xFF) {
-        return 1;
-    }
-    return 0;
-}
-
-/**
- * Non-standard way of detecting drives, use of this function should be as
- * last resort when nothing else works.
- *
- * This function wont work for ATAPI disks.
- *
- * \param port short Is port to drive/head register for device to check for
- * \param disk_num unsigned char Is disk number for disk to check (0 or 1)
- * \return -1 on err, other non-zero if disk exists, or 0 if disk does not exist
- */
-int ata_nonstd_disk_detect_by_select(short port, unsigned char disk_num)
-{
+int ata_check_disk_by_select(short port, int disk) {
     int ret = 0;
-    int iobyte;
+    int counter;
+    unsigned char iobyte;
 
-    /* Read register, and toggle drive bit */
-    iobyte = inb(port);
-    disk_num << 4;
-    iobyte ^= disk_num;
-    outb(iobyte, port);
-
-    /* Read status register */
-    port += 1;
-    iobyte = ata_delay_in(port);
-
-    /* If __ATA_STAT_CDE is set, then we have a drive. */
-    ret = iobyte & __ATA_STAT_CDE;
-    return ret;
-
-}
-
-/**
- * Non-standard way of detecting drives, use of this function should be as
- * last resort when nothing else works.
- *
- * Detect drive by running device diagnostics command, and then checking
- * error register. Allegedly DD command on existing device sets bit in the reg
- * but I'm not 100% sure of this.
- *
- * \param port short Is status/command port of your drive
- * \return -1 on error, other non-zero if device is detected, 0 if not.
- */
-int ata_nostd_disk_detect_by_dd(short port) {
-    if (ata_device_wait(port)) {
-        return -1;
-    }
-
-    /* Write device diagnostics command */
-    outb(0x90, port);
-
-    /* Read error register */
-    port -= 6;
-    ret = inb(port);
-
-    return (ret == 0xFF);
-}
-
-/**
- * Detect ATA disk by Identify command, this seems to be the correct way to
- * do ata disk detection.
- *
- * \param port short Is IO Base for device to detect
- * \param type unsigned char Is primary/secondary device ID (0xA0/0xB0)
- * \return -1 on error,
- *          0 if disk is not detected
- *          1 if disk is detected, but it's not ATA
- *          2 if some form of ATA disk is detected
- *          3 if detected disk is SATA
- *          4 if detected disk is ATAPI
- */
-int ata_disk_detect_by_id(short port, unsigned char type) {
-    int cnt;
-    short disk_id;
-    unsigned char io_byte;
+    /* Select disk */
+    iobyte = inb(__ATA_DRIVE_HEAD(port));
+    iobyte ^= (disk << 4);
+    outb(__ATA_DRIVE_HEAD(port), iobyte);
     
-    if ((type != 0xA0) && (type != 0xB0)) {
+    /** 
+     * We don't use delayed in here, but simply 3 polls to see if 
+     * wait gets set.
+     *
+     * Wait instead of sleep because wait bit toggle does not generate
+     * interrupt, and we don't want to miss wait bit toggling.
+     */
+    for (counter = 0; counter < 3; counter++) {
+        iobyte = inb(__ATA_STAT(port));
+        if ((iobyte & __ATA_STAT_WAIT) == 1) {
+            ret = 2;
+            break;
+        }
+    }
+    return ret;
+}
+
+/**
+ * Check for disk by issuing DD command, and checking if all of error register
+ * bits are set to 1.
+ *
+ * Very unreliable, but just might work at times :)
+ *
+ * \param port short Is IO Base to ata device to check 
+ * 'return int disk type, 0 for no disk, 2 for ata
+ */
+int ata_check_disk_by_dd(short port) {
+    outb(__ATA_STAT(port), __ATA_CMD_DD);
+    if (inb(__ATA_ERR(port)) == 0xFF) {
+        return 2;
+    }
+    return 0;
+}
+
+/**
+ * Check if given ATA disk exists or not
+ *
+ * \param port short Is IO Base to ata device to check
+ * \param type char is primary/secondary(0xA0/0xB0) ID for disk
+ * \return int disk type, 0 for no disk, 1 for non-ata, 2 for ata, 
+ * 3 for sata, 4 for atapi, and -1 for error.
+ */
+int ata_check_disk_by_identity(short port, char type) {
+    int counter;
+    unsigned char iobyte;
+
+    /* First, check that bus isn't floating */
+    if (inb(__ATA_STAT(port)) == 0xFF) {
         return -1;
     }
 
     /* Select drive */
-    port += 6;
-    outb(type, port);
+    outb(__ATA_DRIVE_HEAD(port), type);
 
-    /* set sector count & LBA values to 0 */
-    for (cnt = 0; cnt < 4; cnt++) {
-        outb(0, port - cnt);
+    /* Set sector count & LBA to 0, ports base + 6,5,4,3 */
+    for (counter = 4; counter > 0; counter--) {
+        outb(port + 2 + counter, 0);
     }
 
-    /* Wait for ready */
-    port++;
-    if (ata_device_wait(port)) {
-        return -1;
-    }
+    /* Send IDENTIFY command */
+    outb(__ATA_STAT(port), __ATA_CMD_IDENTIFY);
+    iobyte = ata_delay_in(__ATA_STAT(port));
 
-    /* Send identify command */
-    outb(0xEC, port);
-    io_byte = ata_delay_in(port);
-    
-    /* If status register == 0, disk doesn't exist */
-    if (io_byte == 0) {
+    /* If status is 0, disk does not exist */
+    if (iobyte == 0) {
         return 0;
     }
-    
-    /* Wait until disk is ready */
-    if (ata_delay_in(port)) {
-        return -1;
+
+    /* Check for error indicator in status register */
+    iobyte = ata_delay_in(__ATA_STAT(port));
+
+    /* If there is error, then we have sata or atapi disk (most likely) */
+    if ((iobyte & __ATA_STAT_ERR) != 0) {
+        return ata_check_if_sata_or_atapi(port);
     }
-    io_byte = ata_delay_in(port);
 
-    /* If error bit in status register is set, then disk is sata or atapi */
-    if ((io_byte & __ATA_STAT_ERR) == 1) {
-        /* Disk is sata or ATAPI */
-        port -= 3;
-        io_byte = inb(port);
-        disk_id = ((short)io_byte << 8);
-        io_byte = inb(port--);
-        disk_id |= io_byte;
-
-        if (disk_id == 0x3c3c) {
-            return 3;
-        }
-        if (disk_id == 0x14eb) {
-            return 4;
+    /**
+     * It was not sata or atapi disk, keep polling until status bit 7
+     * (__ATA_STAT_WAIT) is clear or timeout is reached.
+     *
+     */ 
+    for (counter = 0; counter < 2000; counter++) {
+        if ((inb(__ATA_STAT(port)) & __ATA_STAT_WAIT) == 0) {
+            goto wait_done;
         }
     }
-    
+    /* Time'd out :( */
+    return -1;
+
+wait_done:
+    /**
+     * Read LBA low & mid values, if they're not set to zero, this is
+     * non-ata disk
+     */
+    if (inb(__ATA_CYL_LO(port)) != 0) {
+        return __ATA_DISK_ID_INTERNAL_NON_ATA;
+    }
+    if (inb(__ATA_CYL_HI(port)) != 0) {
+        return __ATA_DISK_ID_INTERNAL_NON_ATA;
+    }
+
+    return __ATA_DISK_ID_INTERNAL_ATA;
+}
+
+/**
+ * Helper function to show users pretty messages. 
+ *
+ * \param type Int what kind of disk was detected
+ * \return pointer to disk id string related to disk id
+ */
+char *ata_get_disk_str_by_id(int id) {
+    switch (id) {
+        case(__ATA_DISK_ID_INTERNAL_ATAPI):
+            return __ATA_DISK_ID_STR_ATAPI;
+        case(__ATA_DISK_ID_INTERNAL_SATA):
+            return __ATA_DISK_ID_STR_SATA;
+        case(__ATA_DISK_ID_INTERNAL_ATA):
+            return __ATA_DISK_ID_STR_ATA;
+        default:
+            return __ATA_DISK_ID_STR_NON_ATA;
+    }
+}
+
+/* TODO: doc */
+int detect_ata_disks(void) {
+    uint64_t stat = 0;
+    int ports[] = {0x1f0, 0x170, 0x1e8, 0x168};
+    int ret;
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        sc_do_hardware_ioperm(ports[i], 10, 1, &stat);
+        ret = ata_check_disk_by_identity(ports[i], 0xA0);
+        if (!ret) {
+            ret = ata_check_disk_by_select(ports[i], 0);
+        }
+        if (!ret) {
+            ret = ata_check_disk_by_dd(ports[i]);
+        }
+
+        printf("ATA Disk detect for device 0x%x: ", ports[i]);
+        switch (ret) {
+            case(-1):
+                printf("failed! Bus floats.\n");
+                break;
+            case(0):
+                printf("disk not attached\n");
+                break;
+            default:
+                printf("%s disk detected\n", ata_get_disk_str_by_id(ret));
+                break;
+        }
+    }
+}
 
