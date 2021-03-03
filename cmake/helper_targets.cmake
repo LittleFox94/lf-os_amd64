8ef7f9c8c9b42e978eb41ba2526b3b7a55b72ace
Mara Sophie Grosch <littlefox@lf-net.org>
2021-03-03 04:47:11 +0100

    Boot with clover
    this is hacky

--- cmake/helper_targets.cmake
+++ cmake/helper_targets.cmake
@@ -4,7 +4,17 @@ find_program(kvm  NAMES kvm                DOC "QEMU for x86_64 emulation with K
 find_program(gdb  NAMES gdb                DOC "GNU debugger")
 
 set(QEMU_MEMORY 512M)
-set(QEMU_FLAGS -bios ${ovmf_firmware} -drive format=raw,file=hd.img,if=none,id=boot_drive -device nvme,drive=boot_drive,serial=1234 -m ${QEMU_MEMORY} -d int,guest_errors --serial file:log.txt -s)
+set(QEMU_FLAGS_COMMON -m ${QEMU_MEMORY} -d int,guest_errors --serial file:log.txt -s)
+set(QEMU_FLAGS_UEFI ${QEMU_FLAGS_COMMON} -drive format=raw,file=hd.img,if=none,id=boot_drive -device nvme,drive=boot_drive,serial=1234 -bios ${ovmf_firmware})
+set(QEMU_FLAGS_BIOS ${QEMU_FLAGS_COMMON} -drive format=raw,file=hd.img,if=none,id=boot_drive -device ide-hd,drive=boot_drive,serial=1234)
+
+set(qemu_boot_mode "uefi" CACHE STRING "Boot with uefi or bios?")
+
+if(qemu_boot_mode STREQUAL "uefi")
+    set(QEMU_FLAGS ${QEMU_FLAGS_UEFI})
+elseif(qemu_boot_mode STREQUAL "bios")
+    set(QEMU_FLAGS ${QEMU_FLAGS_BIOS})
+endif()
 
 add_custom_target(debug
     COMMAND ${qemu} ${QEMU_FLAGS} --daemonize -S
--- cmake/images.cmake
+++ cmake/images.cmake
@@ -6,13 +6,19 @@ find_program(mformat NAMES mformat DOC "mformat program for creating MS-DOS file
 find_program(mcopy   NAMES mcopy   DOC "mcopy program for copying files on MS-DOS formated disk images")
 
 math(EXPR image_size_hd_sectors "${image_size_hd} * 1024 * 1024 / 512")
-math(EXPR boot_fs_sectors "${image_size_hd_sectors} - 2048")
+math(EXPR bootfs_start_sectors "2048")
+math(EXPR bootfs_start "${bootfs_start_sectors} * 512")
+math(EXPR bootfs_sectors "${image_size_hd_sectors} - ${bootfs_start_sectors}")
+math(EXPR bootfs_physical_drive "${bootfs_start} + 64")
 
 add_custom_target(hd.img
     COMMAND ${dd} if=/dev/zero of=hd.img bs=512 count=${image_size_hd_sectors}
-    COMMAND ${sgdisk}  -Z -o -n 1:2048:${image_size_headers} -t 1:ef00 hd.img
-    COMMAND ${mformat} -i hd.img@@1M -F -L ${boot_fs_sectors}
-    COMMAND ${mcopy}   -i hd.img@@1M -sbnmv ${CMAKE_BINARY_DIR}/shared/* ::/
+    COMMAND ${sgdisk}  -Z -o -n 1:${bootfs_start_sectors}:${image_size_headers} -t 1:ef00 hd.img -A 1:set:2
+    COMMAND ${mformat} -i hd.img@@1M -F -L ${bootfs_sectors} -H ${bootfs_start_sectors} -B ${CMAKE_SOURCE_DIR}/packaging/CloverV2/boot1f32
+    COMMAND ${mcopy}   -i hd.img@@1M -sbnmv ${CMAKE_BINARY_DIR}/shared/bootfs/* ::/
+
+    COMMAND ${mcopy} -i hd.img@@1M -sbnmv ${CMAKE_SOURCE_DIR}/packaging/CloverV2/boot6 ::/boot
+    COMMAND ${dd} if=${CMAKE_SOURCE_DIR}/packaging/CloverV2/boot0af of=hd.img bs=1 count=440 conv=notrunc
 )
 
 add_custom_target(hd.img.xz
