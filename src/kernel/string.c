6956be8f38bf3ddc24880aa9fbcb60cf12bc4070
Mara Sophie Grosch <littlefox@lf-net.org>
2021-05-18 06:36:58 +0200

    kernel/string: hand buffer_size to kvsnprintf
    wtf?

--- src/kernel/string.c
+++ src/kernel/string.c
@@ -305,7 +305,7 @@ int kvsnprintf(char* buffer, int buffer_size, const char* format, va_list args) 
 int ksnprintf(char* buffer, int buffer_size, const char* format, ...) {
     va_list args;
     va_start(args, format);
-    int count = kvsnprintf(buffer, 512, format, args);
+    int count = kvsnprintf(buffer, buffer_size, format, args);
     va_end(args);
 
     return count;
