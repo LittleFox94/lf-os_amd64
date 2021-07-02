f4887b07ecd1ce8d6ce7e84b3670e15ea05f461b
Mara Sophie Grosch <littlefox@lf-net.org>
2021-07-02 14:26:16 +0200

    cmake/images: disable component packages
    Only build LF OS package, the other subprojects handle their packages on their own

--- cmake/images.cmake
+++ cmake/images.cmake
@@ -20,46 +20,29 @@ add_custom_target(hd.img.xz
     COMMAND ${xz} -k ${CMAKE_BINARY_DIR}/hd.img
 )
 
-set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME main)
-
 install(DIRECTORY ${CMAKE_BINARY_DIR}/shared/EFI/LFOS DESTINATION /boot/efi/EFI)
 install(DIRECTORY ${CMAKE_BINARY_DIR}/shared/LFOS     DESTINATION /boot/efi)
 install(PROGRAMS util/osprobe RENAME 20lfos           DESTINATION /usr/lib/os-probes/mounted/efi)
 
-install(DIRECTORY ${toolchain}/ DESTINATION /opt/lf_os USE_SOURCE_PERMISSIONS COMPONENT toolchain)
-
-set(toolchain_bak ${toolchain})
-
-set(toolchain /opt/lf_os)
-configure_file(cmake/cmake.toolchain.in cmake.toolchain @ONLY)
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/cmake.toolchain DESTINATION ${toolchain}/etc                                               COMPONENT toolchain)
-install(FILES cmake/cmake.platform                        DESTINATION ${toolchain}/share/cmake/Modules/Platform RENAME "LF-OS.cmake" COMPONENT toolchain)
-
-set(toolchain ${toolchain_bak})
-
-set(CPACK_PACKAGE_CONTACT "Mara Sophie Grosch <littlefox@lf-net.org>")
+set(CPACK_PACKAGE_NAME lf_os)
+set(CPACK_PACKAGE_CONTACT  "Mara Sophie Grosch <littlefox@lf-net.org>")
 set(CPACK_PACKAGE_HOMEPAGE "https://praios.lf-net.org/littlefox/lf-os_amd64")
-set(CPACK_GENERATOR        "DEB")
+
+set(CPACK_GENERATOR        "DEB;TXZ")
 set(CPACK_SOURCE_GENERATOR "")
 
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Bootable LF OS system")
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
 set(CPACK_DEBIAN_PACKAGE_CONTROL_STRICT_PERMISSION ON)
-set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_COMPRESSION_TYPE   xz)
+set(CPACK_DEBIAN_PACKAGE_SECTION    kernel)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "grub-efi-amd64, os-prober")
 
-set(CPACK_DEBIAN_MAIN_PACKAGE_NAME lf_os)
-set(CPACK_DEBIAN_MAIN_PACKAGE_SECTION kernel)
-set(CPACK_DEBIAN_MAIN_PACKAGE_DEPENDS "grub-efi-amd64, os-prober")
-set(CPACK_DEBIAN_MAIN_PACKAGE_CONTROL_EXTRA
+set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
     ${CMAKE_SOURCE_DIR}/packaging/preinst
     ${CMAKE_SOURCE_DIR}/packaging/postinst
     ${CMAKE_SOURCE_DIR}/packaging/postrm
 )
 
-set(CPACK_DEBIAN_TOOLCHAIN_PACKAGE_NAME lf_os-dev)
-set(CPACK_DEBIAN_TOOLCHAIN_PACKAGE_RECOMMENDS "cmake (>= 3.10), ninja")
-
 include(CPack)
-include(CPackComponent)
-cpack_add_component(main      DESCRIPTION "Bootable LF OS system")
-cpack_add_component(toolchain DESCRIPTION "Toolchain for LF OS programs")
