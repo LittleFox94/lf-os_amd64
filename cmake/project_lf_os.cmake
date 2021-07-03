54a0673b43e14bfd3741d65981a60f8deec67a70
Mara Sophie Grosch <littlefox@lf-net.org>
2021-07-03 17:19:03 +0200

    Only build sysroot if location not set manually
    Also bump cmake_minimum_required to 3.15

--- cmake/project_lf_os.cmake
+++ cmake/project_lf_os.cmake
@@ -1,8 +1,20 @@
+set(lf_os_sysroot_external OFF)
+if(DEFINED lf_os_sysroot AND NOT DEFINED current_stage)
+    set(lf_os_sysroot_external ON)
+endif()
+
 include(config)
 
 set(staging_shared_install ON)
 include(staging)
-multistage(sysroot libc stdlibs lowlevel userspace)
+
+set(stages lowlevel userspace)
+
+if(NOT lf_os_sysroot_external)
+    list(PREPEND stages sysroot libc stdlibs)
+endif()
+
+multistage(${stages})
 
 if(${current_stage} STREQUAL "root")
     include(tests)
