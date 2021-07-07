88d58876247eb754831b7e4686a6a3ed8a712fb4
Mara Sophie Grosch <littlefox@lf-net.org>
2021-07-07 14:17:54 +0200

    Migrated kernel test slab to C++

---
+++ src/kernel/tests/test_slab.cxx
@@ -0,0 +1,25 @@
+#include <lfostest.h>
+#include <gtest/gtest.h>
+
+namespace LFOS {
+    extern "C" {
+        #include <slab.c>
+    }
+
+    TEST(KernelSlab, Simple) {
+        const size_t size = 1024 * 1024;
+
+        void* memory = malloc(size);
+        init_slab((ptr_t)memory, (uint64_t)memory + size, 1024);
+
+        SlabHeader* slab = (SlabHeader*)memory;
+        ptr_t alloc1 = slab_alloc(slab);
+
+        EXPECT_EQ(alloc1, (uint64_t)memory + 1024) << "correct pointer returned";
+
+        SlabIndexType idx2 = slab_index(slab, (uint64_t)memory + 2048);
+        EXPECT_EQ(idx2, 1) << "correct index returned";
+
+        free((void*)memory);
+    }
+}
