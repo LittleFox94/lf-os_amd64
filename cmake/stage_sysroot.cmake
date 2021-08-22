244d5aae6549f19737faa68895e04bf955c45572
Mara Sophie Grosch <littlefox@lf-net.org>
2021-08-22 14:25:37 +0200

    stage_sysroot: add sys/known_services.h header

--- cmake/stage_sysroot.cmake
+++ cmake/stage_sysroot.cmake
@@ -3,6 +3,7 @@ include(helper_targets)
 install(
     FILES
         src/include/sys/errno-defs.h
+        src/include/sys/known_services.h
         src/include/message_passing.h
         src/include/arch/${architecture}/io.h
         ${CMAKE_CURRENT_BINARY_DIR}/syscalls.h
