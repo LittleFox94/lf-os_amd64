0ead988f38613e722e0e48bd9fd27e0a1f14ee27
Mara Sophie Grosch <littlefox@lf-net.org>
2022-02-22 19:40:06 +0100

    Change mformat command line

--- cmake/images.cmake
+++ cmake/images.cmake
@@ -11,7 +11,7 @@ math(EXPR boot_fs_sectors "${image_size_hd_sectors} - 2048")
 add_custom_target(hd.img
     COMMAND ${dd} if=/dev/zero of=hd.img bs=512 count=${image_size_hd_sectors}
     COMMAND ${sgdisk}  -Z -o -n 1:2048:${image_size_headers} -t 1:ef00 hd.img
-    COMMAND ${mformat} -i hd.img@@1M -F -L ${boot_fs_sectors}
+    COMMAND ${mformat} -i hd.img@@1M -F -T ${boot_fs_sectors}
     COMMAND ${mcopy}   -i hd.img@@1M -sbnmv ${CMAKE_BINARY_DIR}/shared/* ::/
 )
 
