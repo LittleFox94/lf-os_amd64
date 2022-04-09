64aa1258229d9e39cb59c584cc026fb43408d404
Mara Sophie Grosch <littlefox@lf-net.org>
2022-04-09 19:35:22 +0200

    errno-defs.h: add EINTR and EISDIR

--- src/include/sys/errno-defs.h
+++ src/include/sys/errno-defs.h
@@ -4,6 +4,7 @@
 #define EPERM         1
 #define ENOENT        2
 #define ESRCH         3
+#define EINTR         4
 #define EBADF         9
 #define ECHILD       10
 #define EAGAIN       11
@@ -12,6 +13,7 @@
 #define EBUSY        16
 #define EEXIST       17
 #define ENOTDIR      20
+#define EISDIR       21
 #define EINVAL       22
 #define ENFILE       23
 #define EFBIG        27
