978ff04fcc65aa0d168a16310803b8f37eaf0c18
k4m1 <k4m1@pm.me>
2023-12-09 17:49:13 +0200

    Rename 32-bit in- and out functions

--- src/include/arch/amd64/io.h
+++ src/include/arch/amd64/io.h
@@ -11,7 +11,7 @@ static inline void outw(uint16_t port, uint16_t data) {
     asm volatile("outw %0, %1"::"a"(data), "Nd"(port));
 }
 
-static inline void outd(uint16_t port, uint32_t data) {
+static inline void outl(uint16_t port, uint32_t data) {
     asm volatile("outl %0, %1"::"a"(data), "Nd"(port));
 }
 
@@ -27,7 +27,7 @@ static inline uint16_t inw(uint16_t port) {
     return ret;
 }
 
-static inline uint32_t ind(uint16_t port) {
+static inline uint32_t inl(uint16_t port) {
     uint32_t ret;
     asm volatile("inl %1, %0":"=a"(ret):"Nd"(port));
     return ret;
