LittleFox94/lf-os_amd64

commit 3b1aa72fa617f9b0ccd273d72a04e28e7d480f21

cmake: only rebuild firmware.qcow2 and syscalls.h when needed

cmake/helper_targets.cmake
@@ -9,7 +9,8 @@ set(QEMU_FLAGS      -drive format=qcow2,file=firmware.qcow2,if=pflash,readonly=o
 set(QEMU_FLAGS_NVME -drive format=raw,file=hd.img,if=none,id=boot_drive -device nvme,drive=boot_drive,serial=1234)
 set(QEMU_FLAGS_PXE  -netdev user,id=net0,tftp=${CMAKE_BINARY_DIR}/shared,bootfile=/EFI/LFOS/BOOTX64.efi -device virtio-net,netdev=net0,romfile=)
 
-add_custom_target(firmware.qcow2
+add_custom_command(
+    OUTPUT firmware.qcow2
     COMMAND ${qemu_img} create -o backing_file=${ovmf_firmware} -o backing_fmt=raw -o cluster_size=512 -f qcow2 firmware.qcow2
     DEPENDS ${ovmf_firmware}
 )
@@ -53,10 +54,10 @@ add_custom_target(run-kvm
     USES_TERMINAL
 )
 
-add_custom_target(
-    syscalls.h  ALL
-    src/syscall-generator.pl src/syscalls.yml ${PROJECT_BINARY_DIR}/syscalls.h user
-    SOURCES
+add_custom_command(
+    OUTPUT syscalls.h
+    COMMAND src/syscall-generator.pl src/syscalls.yml ${PROJECT_BINARY_DIR}/syscalls.h user
+    DEPENDS
         src/syscalls.yml
         src/syscall-generator.pl
     COMMENT           "Generating userspace syscall bindings"

commit 9883880b00c6f41a01c247591354474e833853d6

fix building userspace syscall bindings
Broken by accident in 3b1aa72fa617f9b0ccd273d72a04e28e7d480f21

cmake/helper_targets.cmake
@@ -54,16 +54,6 @@ add_custom_target(run-kvm
     USES_TERMINAL
 )
 
-add_custom_command(
-    OUTPUT syscalls.h
-    COMMAND src/syscall-generator.pl src/syscalls.yml ${PROJECT_BINARY_DIR}/syscalls.h user
-    DEPENDS
-        src/syscalls.yml
-        src/syscall-generator.pl
-    COMMENT           "Generating userspace syscall bindings"
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-)
-
 add_custom_target(doc
     COMMAND doxygen ${CMAKE_BINARY_DIR}/Doxyfile
     DEPENDS Doxyfile syscalls.h

cmake/stage_sysroot.cmake
@@ -1,12 +1,24 @@
 include(helper_targets)
 
+add_custom_command(
+    OUTPUT ${PROJECT_BINARY_DIR}/syscalls.h
+    COMMAND src/syscall-generator.pl src/syscalls.yml ${PROJECT_BINARY_DIR}/syscalls.h user
+    DEPENDS
+        src/syscalls.yml
+        src/syscall-generator.pl
+    COMMENT           "Generating userspace syscall bindings"
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+)
+
+add_custom_target(generate_syscalls.h ALL DEPENDS ${PROJECT_BINARY_DIR}/syscalls.h)
+
 install(
     FILES
         src/include/sys/errno-defs.h
         src/include/sys/known_services.h
         src/include/sys/message_passing.h
         src/include/arch/${architecture}/io.h
-        ${CMAKE_CURRENT_BINARY_DIR}/syscalls.h
+        ${PROJECT_BINARY_DIR}/syscalls.h
     DESTINATION
         ${lf_os_sysroot}/include/sys/
 )